2c3c07893bfb8d2a082653435d26059975dae63e
Sp1keDesktop <parmar.dhaval1337@gmail.com>
2019-03-28 12:14:46 +0530

    adding the 1st commit in Hub

---
+++ phpinfo.php
@@ -0,0 +1,8 @@
+#This is the sample file
+<?php
+// Show all information, defaults to INFO_ALL
+phpinfo();
+// Show just the module information.
+// phpinfo(8) yields identical results.
+phpinfo(INFO_MODULES);
+?>
